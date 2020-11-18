c9d3f67329705fc512e8904ec9a985328e3111b5
nickfern <nickfernandez1999@gmail.com>
2020-11-17 21:51:25 -0700

    11/17 /customer /customer/login /customer/me

--- index.js
+++ index.js
@@ -15,25 +15,26 @@ app.use(cors())
 
 
 
-app.post("/reviews", auth,  async (req,res)=>{
+app.post("/order", auth,  async (req,res)=>{
 
     try{
-        var movieFK = req.body.movieFK;
-        var summary = req.body.summary;
-        var rating = req.body.rating;
+        var CustomerFK = req.body.CustomerFK;
+        var ConcertFK = req.body.ConcertFK;
+        var OrderDate = req.body.OrderDate;
+        var AdmissionType = req.body.AdmissionType;
     
-        if(!movieFK || !summary || !rating){
+        if(!CustomerFK || !ConcertFK || !OrderDate || !AdmissionType){
             res.status(400).send("bad request")
         }
 
-        summary = summary.replace("'","''")
+        //summary = summary.replace("'","''")
     
         // console.log("here is the contact in /reviews",req.contact)
         // res.send("here is your response")}
     
-        let insertQuery = `INSERT INTO Review(Summary, Rating, MovieFK, ContactFK)
-        OUTPUT inserted.ReviewPK, inserted.Summary, inserted.Rating, inserted.MovieFK
-        VALUES('${summary}','${rating}','${movieFK}',${req.contact.ContactPK})`
+        let insertQuery = `INSERT INTO Order(CustomerFK, AdmissionType, OrderDate, ConcertFK)
+        OUTPUT inserted.TicketID, inserted.CustomerFK, inserted.OrderDate, inserted.AdmissionType
+        VALUES('${AdmissionType}','${OrderDate}','${ConcertFK}',${req.Customer.CustomerID})`
 
         let insertedReview = await db.executeQuery(insertQuery)
 
@@ -41,20 +42,20 @@ app.post("/reviews", auth,  async (req,res)=>{
         res.status(201).send(insertedReview[0])
     }
     catch(error){
-        console.log("error in POST /reviews", error)
+        console.log("error in POST /order", error)
         res.status(500).send()
     }
 })
 
-app.get('/contacts/me', auth, (req,res)=>{
-    res.send(req.contact)
+app.get('/Customer/me', auth, (req,res)=>{
+    res.send(req.Customer)
 })
 
 app.get("/hi",(req,res)=>{
     res.send("hello world")
 })
 
-app.post("/contacts/login", async (req, res)=>{
+app.post("/Customer/login", async (req, res)=>{
     //console.log(req.body)
 
     var email=req.body.email;
@@ -65,14 +66,14 @@ app.post("/contacts/login", async (req, res)=>{
     }
 
     //check that user email exists in database
-    var query = `select * from contact where Email = '${email}'`
+    var query = `select * from Customer where Email = '${email}'`
 
     let result;
 
     try{
         result = await db.executeQuery(query);
     }catch(myError){
-        console.log('error in /contacts/login: ', myError);
+        console.log('error in /Customer/login: ', myError);
         return res.status(500).send()
     }
 
@@ -92,23 +93,23 @@ app.post("/contacts/login", async (req, res)=>{
 
     //generate a token
 
-    let token  = jwt.sign({pk: user.ContactPK}, config.JWT, {expiresIn: '60 minutes'})
+    let token  = jwt.sign({pk: user.CustomerID}, config.JWT, {expiresIn: '60 minutes'})
 
     //console.log(token)
 
     //save token in database and send token and user info back to user
-    let setTokenQuery = `UPDATE Contact SET Token = '${token}' 
-    WHERE ContactPK = ${user.ContactPK}`
+    let setTokenQuery = `UPDATE Customer SET Token = '${token}' 
+    WHERE CustomerID = ${user.CustomerID}`
 
     try{
         await db.executeQuery(setTokenQuery)
         res.status(200).send({
             token: token,
             user: {
-                NameFirst: user.NameFirst,
-                NameLast: user.NameLast,
+                FirstName: user.FirstName,
+                LastName: user.LastName,
                 Email: user.Email,
-                ContactPK: user.ContactPK
+                CustomerID: user.CustomerID
             }
         })
     }
@@ -118,50 +119,51 @@ app.post("/contacts/login", async (req, res)=>{
     }
 })
 
-app.post("/contacts", async (req,res)=>{
-    //res.send("creating user")
-    //console.log("request body", req.body)
+app.post("/Customer", async (req,res)=>{
+    res.send("creating user")
+    console.log("request body", req.body)
 
-    var nameFirst = req.body.nameFirst;
-    var nameLast = req.body.nameLast;
-    var email = req.body.email;
-    var password = req.body.password;
+    var FirstName = req.body.FirstName;
+    var LastName = req.body.LastName;
+    var Email = req.body.Email;
+    var Password = req.body.Password;
+    var DOB = req.body.DOB;
 
-    if(!nameFirst || !nameLast || !email || !password){
+    if(!FirstName || !LastName || !Email || !Password){
         return res.status(400).send("bad request")
     }
 
-    nameFirst = nameFirst.replace("'","''")
-    nameLast = nameLast.replace("'","''")
+    FirstName = FirstName.replace("'","''")
+    LastName = LastName.replace("'","''")
 
-    var emailCheckQuery = `SELECT email
-    from contact 
-    where email = '${email}'`
+    var emailCheckQuery = `SELECT Email
+    from Customer 
+    where Email = '${Email}'`
 
     var existingUser = await db.executeQuery(emailCheckQuery)
 
-    var hashedPassword = bcrypt.hashSync(password)
+    var hashedPassword = bcrypt.hashSync(Password)
     //console.log("existing user", existingUser)
     if(existingUser[0]){
         return res.status(409).send('Please enter a different email.')
     }
 
-    var insertQuery = `insert into contact(NameFirst,NameLast,Email,Password)
-    values('${nameFirst}','${nameLast}','${email}','${hashedPassword}')`
+    var insertQuery = `insert into Customer(FirstName,LastName,Email,Password,DateOfBirth)
+    values('${FirstName}','${LastName}','${Email}','${hashedPassword}', '${DOB}')`
 
     db.executeQuery(insertQuery)
     .then(()=>{res.status(201).send()})
     .catch((err)=>{
-        console.log("error in POST /contacts",err)
+        console.log("error in POST /Customer",err)
         res.status(500).send()
     })
 })
 
-app.get("/movies",(req,res)=>{
+app.get("/Concert1",(req,res)=>{
     //get data from database
-    db.executeQuery(`SELECT * FROM movie
+    db.executeQuery(`SELECT * FROM Concert1
      LEFT JOIN genre
-     ON genre.GenrePK = movie.GenreFK`)
+     ON genre.GenreID = Concert1.GenreFK`)
      .then((result)=>{
          res.status(200).send(result)
      })
@@ -170,25 +172,25 @@ app.get("/movies",(req,res)=>{
          res.status(500).send()
      })
 })
-app.get("/movies/:pk", (req,res)=>{
+app.get("/Concert1/:pk", (req,res)=>{
     var pk = req.params.pk
     console.log("my PK:" , pk)
 
     var myQuery = `select *
-    from movie
-    left join genre
-    on genre.GenrePK = movie.GenreFK
-    Where moviePK = ${pk}`
+    from Concert1
+    left join Genre
+    on Genre.GenreID = Concert1.GenreFK
+    Where ConcertID = ${pk}`
 
     db.executeQuery(myQuery)
-        .then((movies)=>{
+        .then((concerts)=>{
             //console.log("Movies: ",movies)
-            if(movies[0]){
-                res.send(movies[0])
+            if(concerts[0]){
+                res.send(concerts[0])
             }else{res.status(404).send('bad request')}
         })
         .catch((err)=>{
-            console.log("Error in /movies/pk", err)
+            console.log("Error in /Concert1/pk", err)
             res.status(500).send()
         })
 })
--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -16,20 +16,20 @@ const auth = async(req,res,next)=>{
         let decodedToken = jwt.verify(myToken, config.JWT) 
         //console.log(decodedToken)
 
-        let contactPK = decodedToken.pk;
-        console.log(contactPK)
+        let CustomerID = decodedToken.pk;
+        console.log(CustomerID)
 
 
         //2. compare token with database token
-        let query = `SELECT ContactPK, NameFirst, NameLast, Email
-        FROM Contact
-        WHERE ContactPK = ${contactPK} and Token = '${myToken}'`
+        let query = `SELECT CustomerID, FirstName, LastName, Email
+        FROM Customer
+        WHERE CustomerID = ${CustomerID} and Token = '${myToken}'`
 
         let returnedUser = await db.executeQuery(query)
         console.log(returnedUser)
         //3. save user information in the request
     if(returnedUser[0]){
-        req.contact = returnedUser[0];
+        req.Customer = returnedUser[0];
         next()
     }
     else{res.status(401).send('authentication failed.')}
